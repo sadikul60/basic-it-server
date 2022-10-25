3777ed5d323b72b4a20759b7e03bab7dcd08a9a0
mdsadikul <mdsadikulislam891@gamil.com>
2022-10-25 12:59:00 +0600

    Added course.json data create

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/course.json')
+
 app.get('/', (req, res) => {
     res.send('Basic IT server is running')
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log(`Basic IT running on port, ${port}`)
