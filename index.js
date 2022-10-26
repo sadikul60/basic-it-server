1508e3aac2ec8c2d601360701277e2b176760cd2
mdsadikul <mdsadikulislam891@gamil.com>
2022-10-26 10:33:53 +0600

    Added category id in index.js

--- index.js
+++ index.js
@@ -9,27 +9,38 @@ app.use(cors());
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
 
+
+
 app.get('/', (req, res) => {
     res.send('Basic IT server is running')
 });
+
+
 
 app.get('/categories', (req, res) => {
     res.send(categories);
 });
 
-app.get('/categories/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    
-    const category = categories.find(c => c.id === id);
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const category = courses.find(course => course.category_id === id);
     res.send(category);
-});
+})
+
+
 
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+
+
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
+    console.log(id)
     const selectCourse = courses.find(c => c._id === id);
     res.send(selectCourse);
 })
